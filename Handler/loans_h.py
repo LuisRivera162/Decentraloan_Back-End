1391d41cfc3eec5f257af48f8542a1b958fd3b2e
Hector Rodriguez <hector.rodriguez49@upr.edu>
2021-04-08 17:30:20 -0400

    changes to loan related functions

--- Handler/loans_h.py
+++ Handler/loans_h.py
@@ -18,14 +18,15 @@ class LoansHandler:
         result['monthly_repayment'] = row[9]
         result['balance'] = row[10]
         result['est_total_interest'] = row[11]
+        result['platform'] = row[12]
         return result
 
-    def insert_loan(self, loan_amount, lender, borrower, interest, time_frame):
+    def insert_loan(self, loan_amount, lender, borrower, interest, time_frame, platform):
         # NEED TO HANDLE IF USER EXISTS
         dao = LoansDAO()
         
         try:
-            loan_id = dao.insert_loan(lender, borrower, loan_amount, time_frame, interest/100)
+            loan_id = dao.insert_loan(lender, borrower, loan_amount, time_frame, interest/100, platform)
         except:
             return jsonify("Error processing, query."), 400
 
